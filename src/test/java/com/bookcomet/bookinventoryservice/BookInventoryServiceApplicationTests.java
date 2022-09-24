de08d6f49aeacb9108a82927b37fd908d7f50122
fabio <fabiocosta07@gmail.com>
2022-09-24 16:29:59 -0300

    initial commit

---
+++ src/test/java/com/bookcomet/bookinventoryservice/BookInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookcomet.bookinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
