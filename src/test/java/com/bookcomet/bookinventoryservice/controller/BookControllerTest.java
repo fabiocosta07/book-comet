de057bf6e3d7d668f7ec47b1eba9adb80110ad4a
fabio <fabiocosta07@gmail.com>
2022-09-28 15:30:57 -0300

    cleanup

--- src/test/java/com/bookcomet/bookinventoryservice/controller/BookControllerTest.java
+++ src/test/java/com/bookcomet/bookinventoryservice/controller/BookControllerTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BookController.class)
-class BookControllerTests {
+public class BookControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
